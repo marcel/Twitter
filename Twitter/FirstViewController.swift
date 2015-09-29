6e5c21e4e84925dce38e6a7cd986def9cfc25b32
marcel <github@marcelmolina.com>
2015-09-28 22:06:02 -0700

    Initial Commit

---
+++ Twitter/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Twitter
+//
+//  Created by Marcel Molina on 9/28/15.
+//  Copyright © 2015 Marcel Molina. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
